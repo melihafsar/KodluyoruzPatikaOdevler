7558238b9e9de70dfd2d44fa9355971cdcf49dda
Melih Afsar <melih_afsar@hotmail.com.tr>
2022-08-04 12:09:59 +0300

    To-Do-List: onclick func. removed and edited.

--- Javascript/ToDo-List/index.js
+++ Javascript/ToDo-List/index.js
@@ -4,7 +4,25 @@ let alert = document.querySelector("#alert");
 let works;
 let listID;
 
-toDoFormDOM.addEventListener("submit", submitHandler);
+//calisarak sayfa yuklendigi gibi localStorage taki isleri sayfaya doldurur.
+fillTheList()
+
+//eklenen isler sayfaya doldurulduktan sonra class tanimlamalarinda "button" isminde olanlari dinler. Tiklanirsa id degerini dondurur. 
+Array.prototype.forEach.call(document.querySelectorAll('.button'), function (el) {
+    el.addEventListener('click', function () {
+        console.log("10.satir",this.id)
+        removeWork(this.id)
+    });
+});
+
+//eklenen isler sayfaya doldurulduktan sonra class tanimlamalarinda "liElement" isminde olanlari dinler. Tiklanirsa id degerini dondurur. 
+Array.prototype.forEach.call(document.querySelectorAll('.liElement'), function (el) {
+    el.addEventListener('click', function () {
+        console.log("10.satir",this.id)
+        getID(this.id)
+    });
+});
+
 
 class Work {
     constructor(workName, workDetail, workNumber) {
@@ -14,15 +32,20 @@ class Work {
     }
 }
 
+//form doldurulmasinda herhangi bir hata cikmasi durumunda bu hatanin kullaniciya geri bildirim olarak dönmesini saglar.
 let alertFunction = (title, message, className = "warning") => {
     return `
-        <div class="alert alert-${className} alert-dismissible fade show" role="alert">
-            <strong> ${title}!  </strong>${message}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
+    <div class="alert alert-${className} alert-dismissible fade show" role="alert">
+    <strong> ${title}!  </strong>${message}
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
     `;
 };
 
+//form submit butonuna tiklandiginda calisir ve is eklenir.
+toDoFormDOM.addEventListener("submit", submitHandler);
+
+// Bilgisi gelen isler eklenmis olur.
 function submitHandler(event) {
     event.preventDefault();
     let workName = document.querySelector("#inputWorkName");
@@ -62,12 +85,11 @@ function fillTheList() {
     try {
         works.forEach((element, index) => {
             let li = document.createElement("li");
-            li.innerHTML = `
+            li.innerHTML = ` 
             <div class="container">
             <div class="row">
               <div class="col-11">
-                <li onclick="getID(this.id)" id="li${index}"
-                  class="list-group-item d-flex justify-content-between align-items-start">
+                <li id="li${index}" class="liElement list-group-item d-flex justify-content-between align-items-start">
                   <div class="ms-2 me-auto">
                     <div class="fw-bold">${element.workName}</div>
                     ${element.workDetail}
@@ -76,10 +98,9 @@ function fillTheList() {
                     <span class="badge bg-primary rounded-pill">${element.workNumber}</span>
                   </div>
                 </li>
-
               </div>
               <div class="col-1 m-auto">
-                <button onclick="getButtonID(this.id)" id="button-${index}" type="button" class="btn btn-danger">
+                <button id="button-${index}" type="button" class="button btn btn-danger">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x"
                     viewBox="0 0 16 16">
                     <path
@@ -87,30 +108,10 @@ function fillTheList() {
                     </path>
                   </svg>
                 </button>
-
               </div>
             </div>
-          </div>            
+          </div>
 `
-            {/**
-        
-            `
-            <li onclick="getID(this.id)" id="li${index}"
-                class="list-group-item d-flex justify-content-between align-items-start">
-                <div class="ms-2 me-auto">
-                    <div class="fw-bold"> ${element.workName} </div>
-                    ${element.workDetail}
-        </div>
-        <div class="mt-1">
-              <span class="badge bg-primary rounded-pill">${element.workNumber}</span>    
-                <button onclick="getButtonID(this.id)" id="button-${index}" type="button" class="btn btn-danger">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"></path>
-                  </svg>
-                </button>
-              </div>
-        </li>`;
-        */}
             olList.append(li);
         });
     } catch (error) {
@@ -120,17 +121,14 @@ function fillTheList() {
     paintBoxes();
 }
 
-let getButtonID = (buttonID) => removeWork(buttonID);
-
 function removeWork(buttonID) {
     
     let array = buttonID.split("-");
     let workIndex = array[1];
-    letLiID = `li${workIndex}`;
-    let element = document.querySelector(`#${letLiID}`);
+    let liID = `li${workIndex}`;
+    let element = document.querySelector(`#${liID}`);
     let elementHTML = element.innerHTML;
     let works = getLocal();
-    console.log(works);
     let isAvilable = false;
     let index = -1;
     works.forEach((element, itemNo) => {
@@ -139,7 +137,6 @@ function removeWork(buttonID) {
             index = itemNo;
         }
     });
-    console.log(index);
 
     if (index > -1) {
         works.splice(index, 1);
@@ -147,6 +144,26 @@ function removeWork(buttonID) {
     //set local
     localStorage.setItem("workInfo", JSON.stringify(works));
     
+    //! liste kismi sorunlu
+    listID = getIDLocal()
+    console.log("liste ilk durum: ",listID)
+    console.log("butona göre li elementinin id si",liID)
+    let liIndex = -1
+    listID.forEach((element, itemNo) => {
+        if (!isAvilable) {
+            isAvilable = listID.includes(liID);
+            liIndex = itemNo;
+            console.log(liIndex)
+        }
+    });
+
+    if (liIndex > -1) {
+        listID.splice(liIndex, 1);
+    }
+    console.log("liste son durum: ", listID)
+    //set local
+    localStorage.setItem("listID", JSON.stringify(listID));
+
     //! ListId deki boyanan degerler de guncellenmeli
     let buttonElement = document.querySelector(`#${buttonID}`);
     buttonElement.parentElement.removeChild(buttonElement)
